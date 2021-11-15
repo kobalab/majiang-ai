51801a95ddb53a8912e9579cf3e2c88b74882339
Satoshi Kobayashi <koba@yk.rim.or.jp>
2021-11-16 06:50:56 +0800

    思考ルーチン 0000   - 空の応答を返すのみ

--- legacy/player-0000.js
+++ legacy/player-0000.js
@@ -8,75 +8,39 @@ const Majiang = require('@kobalab/majiang-core');
 
 module.exports = class Player extends Majiang.Player {
 
-    kaiju(kaiju) {
-        super.kaiju(kaiju);
-        if (this._callback) this._callback();
-    }
-
-    qipai(qipai) {
-        super.qipai(qipai);
-        if (this._callback) this._callback();
-    }
-
-    zimo(zimo, gangzimo) {
-        super.zimo(zimo, gangzimo);
-        if (! this._callback) return;
+    action_zimo(zimo, gangzimo) {
         if (zimo.l != this._menfeng) return this._callback();
-        let mianzi;
-        if      (this.select_hule(gangzimo))  this._callback({hule: '-'});
-        else if (this.select_pingju())        this._callback({pingju: '-'});
-        else if (mianzi = this.select_gang()) this._callback({gang: mianzi});
+        let m;
+        if      (this.select_hule(null, gangzimo))
+                                         this._callback({hule: '-'});
+        else if (this.select_pingju())   this._callback({daopai: '-'});
+        else if (m = this.select_gang()) this._callback({gang: m});
         else this._callback({dapai: this.select_dapai()});
     }
 
-    dapai(dapai) {
-        super.dapai(dapai);
-        if (! this._callback) return;
+    action_dapai(dapai) {
         if (dapai.l == this._menfeng) return this._callback();
-        let mianzi;
-        if      (this.select_hule(dapai.p))    this._callback({hule: '-'});
-        else if (mianzi = this.select_fulou()) this._callback({fulou: mianzi});
-        else                                   this._callback();
+        let m;
+        if      (this.select_hule(dapai)) this._callback({hule: '-'});
+        else if (m = this.select_fulou()) this._callback({fulou: m});
+        else                              this._callback();
     }
 
-    fulou(fulou) {
-        super.fulou(fulou);
-        if (! this._callback) return;
+    action_fulou(fulou) {
         if (fulou.l != this._menfeng) return this._callback();
         this._callback({dapai: this.select_dapai()});
     }
 
-    gang(gang) {
-        super.gang(gang);
-        if (! this._callback) return;
+    action_gang(gang) {
         if (gang.l == this._menfeng) return this._callback();
-        if (this.select_hule(gang.m)) this._callback({hule: '-'});
-        else                          this._callback();
+        if (this.select_hule(gang, true)) this._callback({hule: '-'});
+        else                              this._callback();
     }
 
-    kaigang(kaigang) {
-        super.kaigang(kaigang);
-    }
-
-    hule(hule) {
-        super.hule(hule);
-        if (this._callback) this._callback();
-    }
-
-    pingju(pingju) {
-        super.pingju(pingju);
-        if (this._callback) this._callback();
-    }
-
-    jieju(paipu) {
-        super.jieju(paipu);
-        if (this._callback) this._callback();
-    }
-
-    select_hule(param) {}
+    select_hule(data, hupai) {}
     select_pingju() {}
-    select_fulou(p) {}
-    select_gang(p)  {}
-    select_dapai()  {}
+    select_fulou(dapai) {}
+    select_gang() {}
+    select_dapai() {}
     select_lizhi(p) {}
 }
