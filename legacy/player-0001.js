473e8d577702749f36e69baba4c0ecb9d1c2ca9a
Satoshi Kobayashi <koba@yk.rim.or.jp>
2021-12-09 06:58:04 +0800

    ハイテイの手番のときにテンパイ宣言しないバグを修正

--- legacy/player-0001.js
+++ legacy/player-0001.js
@@ -33,7 +33,11 @@ module.exports = class Player extends Majiang.Player {
     }
 
     action_dapai(dapai) {
-        if (dapai.l == this._menfeng) return this._callback();
+        if (dapai.l == this._menfeng)  {
+            if (this.select_daopai()) this._callback({daopai: '-'});
+            else                      this._callback();
+            return;
+        }
         let m;
         if      (this.select_hule(dapai))      this._callback({hule: '-'});
         else if (m = this.select_fulou(dapai)) this._callback({fulou: m});
